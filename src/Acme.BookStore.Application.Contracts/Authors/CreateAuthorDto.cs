f897c50685b1925af9328a5fd6d3dcbb96a8581a
bh-boostdraft <bh.shin@boostdraft.com>
2025-09-05 09:42:45 +0900

    initial commit

---
+++ src/Acme.BookStore.Application.Contracts/Authors/CreateAuthorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Acme.BookStore.Authors;
+
+[Serializable]
+public class CreateAuthorDto
+{
+    [Required]
+    [StringLength(AuthorConsts.MaxNameLength)]
+    public string Name { get; set; }
+
+    [Required]
+    public DateTime BirthDate { get; set; } = new DateTime(1970, 1, 1);
+
+    public string ShortBio { get; set; }
+}
